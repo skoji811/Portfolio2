6aab553664c867dca43dd8935f71c0e36450fd52
skoji811 <s.koji811@gmail.com>
2022-01-27 13:50:33 +0900

    20210127

---
+++ register/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "name")
+public class User {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "phone")
+	private String phone;
+	
+	@Column(name = "update_date")
+	private Date updateDate;
+	
+	@Column(name = "create_date")
+	private Date createDate;
+	
+	@Column(name = "delete_date")
+	private Date deleteDate;
+	
+	
+}
