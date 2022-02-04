8420b17d99e6aab939a1ffe4772ce574b6c8c8eb
skoji811 <s.koji811@gmail.com>
2022-02-04 11:26:05 +0900

    20220204　詳細画面遷移追加

---
+++ register/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.dto.UserRequest;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+@Controller
+public class UserController {
+
+@Autowired
+private UserService userService;
+
+@GetMapping("/user/list")
+public String displayList(Model model) {
+	List<User> userlist = userService.SearchAll();
+	model.addAttribute("userlist",userlist);
+	return "user/list";
+}
+
+@GetMapping("/user/add")
+public String displayAdd(Model model) {
+	model.addAttribute("userRequest", new UserRequest());
+	return "user/add";
+}
+
+@PostMapping("/user/create")
+public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+    if (result.hasErrors()) {
+        
+        List<String> errorList = new ArrayList<String>();
+        for (ObjectError error : result.getAllErrors()) {
+          errorList.add(error.getDefaultMessage());
+        }
+        model.addAttribute("validationError", errorList);
+        return "user/add";
+      }
+      
+      userService.create(userRequest);
+      return "redirect:/user/list";
+    }
+
+	
+@GetMapping("/user/{id}")
+public String displayView(@PathVariable Long id, Model model) {
+    User user = userService.findById(id);
+    model.addAttribute("userData", user);
+	return "user/view";
+}
+}
