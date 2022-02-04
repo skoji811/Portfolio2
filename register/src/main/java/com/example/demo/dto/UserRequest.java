ab663a4aad1d83b315e24e0fa7583c4df016fe83
skoji811 <s.koji811@gmail.com>
2022-02-04 11:26:34 +0900

    20220204 validation機能追加

---
+++ register/src/main/java/com/example/demo/dto/UserRequest.java
@@ -0,0 +1,33 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+@Data
+public class UserRequest implements Serializable {
+	
+	@NotNull(message ="JANコードを入力してください")
+	@Max(value =20, message ="20文字以内で入力してください")
+	private Long jan;
+	
+	@NotEmpty(message ="メーカー名を入力してください")
+	@Size(max = 100,message ="100文字以内で入力してください")
+	private String manufacturer;
+	
+	@NotEmpty(message ="商品名を入力してください")
+	@Size(max = 100,message ="100文字以内で入力してください")
+	private String name;
+	
+	@NotNull(message ="賞味期間を入力してください")
+	@Max(value =20, message ="20文字以内で入力してください")
+	private Long expiration;
+	
+	
+	
+	
+}
