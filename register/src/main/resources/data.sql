11598319532ba5f98fa4af254579cd48eb924722
skoji811 <s.koji811@gmail.com>
2022-02-04 11:24:05 +0900

    20220204　sqlデータ

---
+++ register/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `sampledb`.`user` (`id`, `name`, `address`, `phone`, `update_date`, `create_date`) VALUES ('1', 'テスト太郎', '東京都品川区1-1', '090-0000-0000', '2021/06/30', '2021/06/30');
+INSERT INTO `sampledb`.`user` (`id`, `name`, `address`, `phone`, `update_date`, `create_date`) VALUES ('2', '榊原次郎', '東京都渋谷区1-1', '080-0000-0000', '2021/06/30', '2021/06/30');
