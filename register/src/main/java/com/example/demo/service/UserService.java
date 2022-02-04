aae15f3d7dcdd5e716ac9ed6f82fbbf439ff4f07
skoji811 <s.koji811@gmail.com>
2022-02-04 11:26:51 +0900

    20220204

--- register/src/main/java/com/example/demo/service/UserService.java
+++ register/src/main/java/com/example/demo/service/UserService.java
@@ -1,14 +1,18 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class UserService {
 
 @Autowired
@@ -16,5 +20,17 @@ private UserRepository userRepository;
 
 public List<User> SearchAll(){
 	return userRepository.findAll();
+}
+
+public void create(UserRequest userRequest) {
+    Date now = new Date();
+    User user = new User();
+    user.setJan(userRequest.getJan());
+    user.setManufacturer(userRequest.getManufacturer());
+    user.setName(userRequest.getName());
+    user.setExpiration(userRequest.getExpiration());
+    user.setCreateDate(now);
+    user.setUpdateDate(now);
+    userRepository.save(user);
 }
 }
