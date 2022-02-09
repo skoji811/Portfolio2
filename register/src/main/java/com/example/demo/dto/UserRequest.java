46d742e9a1ec5f8607a2e9a6cdbecfb9027423dd
skoji811 <s.koji811@gmail.com>
2022-02-10 00:27:16 +0900

    20220210 validationエラー処理

--- register/src/main/java/com/example/demo/dto/UserRequest.java
+++ register/src/main/java/com/example/demo/dto/UserRequest.java
@@ -2,7 +2,7 @@ package com.example.demo.dto;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -12,7 +12,7 @@ import lombok.Data;
 public class UserRequest implements Serializable {
 	
 	@NotNull(message ="JANコードを入力してください")
-	@Max(value =20, message ="20文字以内で入力してください")
+	@Min(value =20, message ="20文字以内で入力してください")
 	private Long jan;
 	
 	@NotEmpty(message ="メーカー名を入力してください")
@@ -24,7 +24,7 @@ public class UserRequest implements Serializable {
 	private String name;
 	
 	@NotNull(message ="賞味期間を入力してください")
-	@Max(value =20, message ="20文字以内で入力してください")
+	@Min(value =20, message ="20文字以内で入力してください")
 	private Long expiration;
 	
 	
