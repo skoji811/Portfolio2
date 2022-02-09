06ff8066831be14f929c40859ea9411fd8219943
skoji811 <s.koji811@gmail.com>
2022-02-10 00:32:46 +0900

    20220210 編集画面からの格納クラス

---
+++ register/src/main/java/com/example/demo/dto/UserUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class UserUpdateRequest extends UserRequest implements Serializable {
+
+	  @NotNull
+	  private Long id;
+}
