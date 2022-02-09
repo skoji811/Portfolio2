a52a8e3f03a08a311ac8a6666c711bc321bb4449
skoji811 <s.koji811@gmail.com>
2022-02-10 00:27:33 +0900

    20220210 編集機能追加

--- register/src/main/java/com/example/demo/service/UserService.java
+++ register/src/main/java/com/example/demo/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dto.UserRequest;
+import com.example.demo.dto.UserUpdateRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
@@ -33,4 +34,18 @@ public void create(UserRequest userRequest) {
     user.setUpdateDate(now);
     userRepository.save(user);
 }
+
+public User findById(Long id) {
+    return userRepository.findById(id).get();
+  }
+
+public void update(UserUpdateRequest userUpdateRequest) {
+    User user = findById(userUpdateRequest.getId());
+    user.setJan(userUpdateRequest.getJan());
+    user.setManufacturer(userUpdateRequest.getManufacturer());
+    user.setName(userUpdateRequest.getName());
+    user.setExpiration(userUpdateRequest.getExpiration());
+    user.setUpdateDate(new Date());
+    userRepository.save(user);
+  }
 }
